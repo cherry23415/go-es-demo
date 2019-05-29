3d4d311e9eca4911739f9eb0feba90592d8ec01a
cherry <cherry@maizuo.com>
2019-05-29 15:05:14 +0800

    es地址改成可配置的

--- src/server/common/es.go
+++ src/server/common/es.go
@@ -2,11 +2,17 @@ package common
 
 import (
 	"github.com/elastic/go-elasticsearch/v6"
+	"github.com/spf13/viper"
 	"log"
+	"strings"
 )
 
 func SetEsClient() {
-	es, err := elasticsearch.NewDefaultClient()
+	//地址，多个逗号间隔
+	addrs := strings.Split(viper.GetString("es.addr"), ",")
+	es, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: addrs,
+	})
 	if err != nil {
 		log.Fatalf("create es client failed, err: %v", err)
 	}
